5020b6f7f5841f7647517d5f0f6a9e843629477d
lucas <lucas.huang@mcttechnology.cn>
2021-06-23 10:59:35 +0800

    第一小节 项目创建和Hero组件的创建

---
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+// 有些元素据位于@Component装饰器中， 会把它加到组件类上，另一些关键性的元素据位于@NgModule装饰器中
+// 最重要的@NgModule装饰器位于顶层类AppModule上
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
