fc22574c6c681005f229f97af4728991fd7b7e53
lucas <lucas.huang@mcttechnology.cn>
2021-06-24 17:57:03 +0800

    第三节 创建hero detail组件

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 // 有些元素据位于@Component装饰器中， 会把它加到组件类上，另一些关键性的元素据位于@NgModule装饰器中
 // 最重要的@NgModule装饰器位于顶层类AppModule上
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
